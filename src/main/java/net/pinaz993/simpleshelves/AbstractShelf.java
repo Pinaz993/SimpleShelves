3d6e49b56557c8d2441ae8e4d1f2bd8c3ed208c0
Patrick Shannon <pinaz993@gmail.com>
2021-07-19 12:37:30 -0400

    Extended AbstractShelf to OakShelf to make the project build. Added AbstractShelf.onUse, with content for testing.

--- src/main/java/net/pinaz993/simpleshelves/AbstractShelf.java
+++ src/main/java/net/pinaz993/simpleshelves/AbstractShelf.java
@@ -4,14 +4,21 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public abstract class AbstractShelf extends HorizontalFacingBlock {
     /**
@@ -197,4 +204,14 @@ public abstract class AbstractShelf extends HorizontalFacingBlock {
         };
     }
     //</editor-fold>
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if(player.getMainHandStack() == ItemStack.EMPTY) {
+            player.playSound(SoundEvents.ENTITY_PLAYER_LEVELUP,1, 1);
+        } else {
+            player.playSound(SoundEvents.ENTITY_PLAYER_HURT, 1, 1);
+        }
+        return ActionResult.SUCCESS;
+    }
 }
---
+++ src/main/java/net/pinaz993/simpleshelves/OakShelf.java
@@ -0,0 +1,12 @@
+package net.pinaz993.simpleshelves;
+
+public class OakShelf extends AbstractShelf{
+    /**
+     * Implementation of Abstract Shelf that uses the :oak_planks texture.
+     *
+     * @param settings: used for super HorizontalFacingBlock
+     */
+    public OakShelf(Settings settings) {
+        super(settings);
+    }
+}
