3a5df4934154360d3a5a126260b1c0170c7ad775
Patrick Shannon <pinaz993@gmail.com>
2021-07-30 09:07:24 -0400

    Finished tests for LocalHorizontalSide. Fixed slight if-chain oversight thanks to tests.

--- src/main/java/net/pinaz993/simpleshelves/LocalHorizontalSide.java
+++ src/main/java/net/pinaz993/simpleshelves/LocalHorizontalSide.java
@@ -4,21 +4,25 @@ import net.minecraft.util.math.Direction;
 
 /**
  * A helper enum to tell right from left from back from front based on what direction a HorizontalFacingBlock is facing.
- * This could have been a private enum, but I want to try out tests. Thus, I have no choice but to make it it's own file.
+ * This could have been a private enum, but I want to try out tests. Thus, I have no choice but to put it in its own file.
  */
 enum LocalHorizontalSide {
     BACK, BOTTOM, FRONT, LEFT, RIGHT, TOP;
 
     /**
-     * See above. Will throw a state exception if fed garbage. TODO: Needs tests.
-     * @param cardinalSide: side in question
-     * @param blockIsFacing: The direction the block is facing. Must be NORTH, EAST, SOUTH, or WEST
+     * See above. Will throw a state exception if fed garbage.
+     * @param cardinalSide The side in question.
+     * @param blockIsFacing The direction the block is facing. Must be NORTH, EAST, SOUTH, or WEST.
      * @return The local side equivalent to cardinalSide.
      */
     public static LocalHorizontalSide getLocalSide(Direction cardinalSide, Direction blockIsFacing) {
-        // Ordered by probability of use.
+        // Ordered by probability of use, except for UP and DOWN, as they don't always work if the front test is run first.
+        // This is only designed for children of HorizontalFacingBlock, so it can't face UP or DOWN.
+        if (blockIsFacing == Direction.UP || blockIsFacing == Direction.DOWN)
+            throw new IllegalStateException(String.format(
+                    "Block cannot face %s", blockIsFacing));
         // The side facing the direction the block is facing is the front,
-        if (cardinalSide == blockIsFacing) return FRONT;
+        else if (cardinalSide == blockIsFacing) return FRONT;
             // 3 o'clock is right,
         else if (cardinalSide == blockIsFacing.rotateYClockwise()) return RIGHT;
             // 9 o'clock is left,
@@ -29,10 +33,6 @@ enum LocalHorizontalSide {
         else if (cardinalSide == Direction.DOWN) return BOTTOM;
             // and the opposite side is back.
         else if (cardinalSide == blockIsFacing.getOpposite()) return BACK;
-            // This is only designed for children of HorizontalFacingBlock, so it can't face UP or DOWN.
-        else if (blockIsFacing == Direction.UP || blockIsFacing == Direction.DOWN)
-            throw new IllegalStateException(String.format(
-                    "Block cannot face %s", blockIsFacing));
             // Finally, throw a verbose exception if you don't know what else to do. I think I've covered everything, but I could be wrong.
         else throw new IllegalStateException(String.format(
                     "Cannot determine local side from %s side of %s facing block.", cardinalSide, blockIsFacing));
