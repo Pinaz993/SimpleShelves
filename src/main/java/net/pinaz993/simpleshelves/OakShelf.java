ea2ce9b869e9f1ddde4705ead3da84d9a865df99
Patrick Shannon <pinaz993@gmail.com>
2021-08-04 10:07:54 -0400

    Don't think I'll need to do anything else with that file.

--- src/main/java/net/pinaz993/simpleshelves/OakShelf.java
+++ src/main/java/net/pinaz993/simpleshelves/OakShelf.java
@@ -3,10 +3,8 @@ package net.pinaz993.simpleshelves;
 public class OakShelf extends AbstractShelf{
     /**
      * Implementation of Abstract Shelf that uses the :oak_planks texture.
-     *
+     * This is as simple as an implementation gets, I think.
      * @param settings: used for super HorizontalFacingBlock
      */
-    public OakShelf(Settings settings) {
-        super(settings);
-    }
+    public OakShelf(Settings settings) {super(settings);}
 }
