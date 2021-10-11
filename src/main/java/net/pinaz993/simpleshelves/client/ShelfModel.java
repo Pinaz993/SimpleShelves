dd631f499e42e101f1d74069c1e0a512be895282
Patrick Shannon <pinaz993@gmail.com>
2021-10-11 11:09:35 -0400

    The 'baking' branch is for experimenting with on the fly model baking for books on shelves.

---
+++ src/main/java/net/pinaz993/simpleshelves/client/ShelfModel.java
@@ -0,0 +1,8 @@
+package net.pinaz993.simpleshelves.client;
+
+import net.fabricmc.fabric.api.renderer.v1.model.FabricBakedModel;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.UnbakedModel;
+
+public class ShelfModel implements UnbakedModel, BakedModel, FabricBakedModel {
+}
