cb29c9cec49dcfb6b45f1ae3a1ec583eba8aa2b2
Patrick Shannon <pinaz993@gmail.com>
2021-10-15 11:09:05 -0400

    Moved wooden shelf classes to a separate package to reduce clutter.

--- src/main/java/net/pinaz993/simpleshelves/woodshelves/WarpedShelf.java
+++ src/main/java/net/pinaz993/simpleshelves/woodshelves/WarpedShelf.java
@@ -1,12 +1,14 @@
-package net.pinaz993.simpleshelves;
+package net.pinaz993.simpleshelves.woodshelves;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
+import net.pinaz993.simpleshelves.AbstractShelf;
+import net.pinaz993.simpleshelves.ShelfEntity;
 import org.jetbrains.annotations.Nullable;
 
-public class BirchShelf extends AbstractShelf {
-    public BirchShelf(Settings settings) {
+public class WarpedShelf extends AbstractShelf {
+    public WarpedShelf(Settings settings) {
         super(settings);
     }
 
