d75400ff60bf6d7eab7dd111dd77846ae77991be
Patrick Shannon <pinaz993@gmail.com>
2021-10-15 10:52:45 -0400

    Reversed rotation of quad vertices to ensure that quads are visible from the proper side.

--- src/main/java/net/pinaz993/simpleshelves/client/BookModel.java
+++ src/main/java/net/pinaz993/simpleshelves/client/BookModel.java
@@ -17,11 +17,12 @@ import static net.minecraft.util.math.MathHelper.abs;
 
 /* Represents the four quads that need to be created to render a book on a shelf. Contains a constructor that randomly
    determines the book's variable qualities, and a method for emitting the proper quads. 12 of these objects exist for
-   every shelf in client-loaded chunks.
+   every shelf in client-loaded chunks. Be ware.
  */
 @SuppressWarnings("deprecation") //Because Mojang abuses @Deprecated. Not the smartest practice, I'll say. Again.
 public class BookModel {
-    // Arrays of strings for identifying textures.
+    //<editor-fold desc="Static Stuff">
+    // Arrays of strings for finding textures.
     private static final String[] ONE_PX_TEXTURE_NAMES = new String[] {
             "simple_shelves:block/book_one_px_alpha"
     };
@@ -46,7 +47,7 @@ public class BookModel {
             "simple_shelves:block/book_paper"
     };
 
-    // Static directions for each quad, before rotation. Horizontal orientation taken from Blockbench. I hope it works.
+    // The direction the various quads should be visible from. I don't know how to use these yet.
     private static final Direction HEAD_DIRECTION = Direction.UP; // Direction for the head (or top edge) quad.
     private static final Direction REAR_COVER_DIRECTION = Direction.WEST; // Direction for the rear cover quad.
     private static final Direction SPINE_DIRECTION = Direction.SOUTH; // Direction for the spine quad.
@@ -88,23 +89,23 @@ public class BookModel {
                 SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE,
                 new Identifier(names[(int) (Math.ceil(random.nextDouble() % 1) * names.length)]));
     }
+    //</editor-fold>
 
     // The height of the book, as determined by an RNG seeded with the coordinates of the block and the seed of the
     // world. Calculated to be on the correct shelf at object creation.
     private final float HEIGHT;
     // The height of the bottom of the book. Calculated to land on the correct shelf at object creation.
     private final float SHELF;
-    private final float DEPTH; // The depth of the book, as determined in the same way as above.
+    private final float DEPTH; // The depth of the book, as determined in the same way as HEIGHT above.
 
-    private final float LEFT_EDGE;
-    private final float RIGHT_EDGE;
-    private final int PIXELS;
-
+    private final float LEFT_EDGE; // The X coordinate of the left edge of the spine, AKA the rear cover.
+    private final float RIGHT_EDGE; // The X coordinate of the right edge of the spine, AKA the front cover.
+    private final int PIXELS; // The width of the book in pixels. 1-4 are the valid values.
 
     private final SpriteIdentifier HEAD; // The texture to use for the head quad.
     private final SpriteIdentifier COVER; // The texture to use for the rear cover, spine, and front cover quads.
 
-    private final int HEAD_HORIZONTAL_OFFSET;
+    private final int HEAD_HORIZONTAL_OFFSET; // An offset to make the paper look different on different books.
 
     /**
      * Given a BookPosition to occupy, randomly determines the height and depth of the book (within static limits), and
@@ -112,12 +113,11 @@ public class BookModel {
      * emission.
      */
     public BookModel(BookPosition bookPosition, Random random){
-        // Is the book on the top shelf?
-        boolean topShelf = bookPosition.getQuadrant() == ShelfQuadrant.ALPHA || bookPosition.getQuadrant() == ShelfQuadrant.BETA;
+        boolean topShelf = bookPosition.getQuadrant() == ShelfQuadrant.ALPHA || // Is the book on the top shelf?
+                bookPosition.getQuadrant() == ShelfQuadrant.BETA;
         // Calculate a random height between MIN_HEIGHT and MAX_HEIGHT. Add .5 if the book is on the top shelf.
         this.HEIGHT = MIN_HEIGHT * random.nextFloat() * (MAX_HEIGHT-MIN_HEIGHT) + (topShelf ? .5f : 0f);
-        // Set the height of SHELF.
-        this.SHELF = topShelf ? 8f / 16f: 1f / 16f;
+        this.SHELF = topShelf ? 8f / 16f: 1f / 16f; // Set the height of SHELF.
         // Calculate a random depth between MIN_DEPTH and MAX_DEPTH.
         this.DEPTH = MIN_DEPTH * random.nextFloat() * (MAX_DEPTH-MIN_DEPTH);
         // Calculate a horizontal offset for the head texture.
@@ -126,11 +126,8 @@ public class BookModel {
         this.LEFT_EDGE = bookPosition.getLeftEdge();
         this.RIGHT_EDGE = bookPosition.getRightEdge();
         this.PIXELS = bookPosition.PIXELS;
-        // Grab random textures. HEAD, from the paper textures:
-        this.HEAD = getRandomSprite(0, random);
-        // and COVER, from the appropriate cover textures.
-        this.COVER = getRandomSprite(bookPosition.PIXELS, random);
-
+        this.HEAD = getRandomSprite(0, random);            // Grab random textures. HEAD, from the paper textures
+        this.COVER = getRandomSprite(bookPosition.PIXELS, random);  // and COVER, from the appropriate cover textures.
     }
 
     /**
@@ -143,60 +140,69 @@ public class BookModel {
      * @param tg: A function for getting sprites from the atlas. Copied from the tutorial.
      */
     public void emitBookQuads(QuadEmitter e, Function<SpriteIdentifier, Sprite> tg) {
-        // TODO: Make the quads visible only from the correct side, if possible.
+        // All points need to be ordered CCW to make sure that they render on the correct side.
+        // I wish I had known that already.
         // Get the sprites.
         Sprite head = tg.apply(HEAD);
         Sprite cover = tg.apply(COVER);
         // Draw the head quad.
         e.pos(0, LEFT_EDGE, HEIGHT, Z_BACK_STOP);
-        e.pos(1, RIGHT_EDGE, HEIGHT, Z_BACK_STOP);
+        e.pos(1, LEFT_EDGE, HEIGHT, DEPTH);
         e.pos(2, RIGHT_EDGE, HEIGHT, DEPTH);
-        e.pos(3, LEFT_EDGE, HEIGHT, DEPTH);
+        e.pos(3, RIGHT_EDGE, HEIGHT, Z_BACK_STOP);
         // Now that we've designated all four corners of the book, we need to set the sprite position for each vertex.
         // Calculate a random horizontal offset for the page texture. Don't go over the edge of the texture.
         e.sprite(0, 0, HEAD_HORIZONTAL_OFFSET, 0);
-        e.sprite(1, 0, HEAD_HORIZONTAL_OFFSET + PIXELS, 0);
+        e.sprite(1, 0, HEAD_HORIZONTAL_OFFSET, 7);
         // The texture dimensions don't change with the random sizing of the books. This causes the texture to be scaled
         // along with the book model.
         e.sprite(2, 0, HEAD_HORIZONTAL_OFFSET + PIXELS, 7);
-        e.sprite(3, 0, HEAD_HORIZONTAL_OFFSET, 7);
-        // Apply the texture and emit.
+        e.sprite(3, 0, HEAD_HORIZONTAL_OFFSET + PIXELS, 0);
+        // Enable texture usage
+        e.spriteColor(0, -1, -1, -1, -1);
+        // Apply the paper texture and emit.
         e.spriteBake(0, head, MutableQuadView.BAKE_ROTATE_NONE).emit();
         // Now we render the rear cover. Vertex coordinates:
         e.pos(0, LEFT_EDGE, HEIGHT, Z_BACK_STOP);
-        e.pos(1, LEFT_EDGE, HEIGHT, DEPTH);
+        e.pos(1, LEFT_EDGE, SHELF, Z_BACK_STOP);
         e.pos(2, LEFT_EDGE, SHELF, DEPTH);
-        e.pos(3, LEFT_EDGE, SHELF, Z_BACK_STOP);
+        e.pos(3, LEFT_EDGE, HEIGHT, DEPTH);
         // Sprite coordinates (No offset this time, as we already know where in the texture this face falls.)
         e.sprite(0, 0, 0, 0);
-        e.sprite(1, 0, 7, 0);
+        e.sprite(1, 0, 0, 7);
         e.sprite(2, 0, 7, 7);
-        e.sprite(3, 0, 0, 7);
+        e.sprite(3, 0, 7, 0);
+        // Enable texture usage
+        e.spriteColor(0, -1, -1, -1, -1);
         // Apply the cover texture and emit.
         e.spriteBake(0, cover, MutableQuadView.BAKE_ROTATE_NONE).emit();
         // Now for the spine. Vertices:
         e.pos(0, LEFT_EDGE, HEIGHT, DEPTH);
-        e.pos(1, RIGHT_EDGE, HEIGHT, DEPTH);
+        e.pos(1, LEFT_EDGE, SHELF, DEPTH);
         e.pos(2, RIGHT_EDGE, SHELF, DEPTH);
-        e.pos(3, LEFT_EDGE, SHELF, DEPTH);
+        e.pos(3, RIGHT_EDGE, HEIGHT, DEPTH);
         // Sprite Coordinates:
         e.sprite(0, 0, 7, 0);
-        e.sprite(1, 0, 7 + PIXELS, 0);
+        e.sprite(1, 0, 7, 7);
         e.sprite(2, 0, 7 + PIXELS, 7);
-        e.sprite(3, 0, 7, 7);
+        e.sprite(3, 0, 7 + PIXELS, 0);
+        // Enable texture usage
+        e.spriteColor(0, -1, -1, -1, -1);
         // Apply the cover texture and emit.
         e.spriteBake(0, cover, MutableQuadView.BAKE_ROTATE_NONE).emit();
         // Lastly, we have the front cover. Vertices:
         e.pos(0, RIGHT_EDGE, HEIGHT, DEPTH);
-        e.pos(1, RIGHT_EDGE, HEIGHT, Z_BACK_STOP);
+        e.pos(1, RIGHT_EDGE, SHELF, DEPTH);
         e.pos(2, RIGHT_EDGE, SHELF, Z_BACK_STOP);
-        e.pos(3, RIGHT_EDGE, SHELF, DEPTH);
+        e.pos(3, RIGHT_EDGE, HEIGHT, Z_BACK_STOP);
         // Sprite coordinates:
         e.sprite(0, 0, 7 + PIXELS, 0);
-        e.sprite(1, 0, 7 + PIXELS + 7, 0);
+        e.sprite(1, 0, 7 + PIXELS, 7);
         e.sprite(2, 0, 7 + PIXELS + 7, 7);
-        e.sprite(3, 0, 7 + PIXELS, 7);
-        // For the final time in this book, apply the cover texture and emit.
+        e.sprite(3, 0, 7 + PIXELS + 7, 0);
+        // Enable texture usage
+        e.spriteColor(0, -1, -1, -1, -1);
+        // For the final time for this book, apply the cover texture and emit.
         e.spriteBake(0, cover, MutableQuadView.BAKE_ROTATE_NONE).emit();
     }
 }
