ed390d4e2b1b23e7261c1c0c8cf12ead33c7b22f
Patrick Shannon <pinaz993@gmail.com>
2021-11-23 14:28:53 -0500

    I want to get baking done, so that I can make shelves waterloggable without doubling the state permutations.

--- src/main/java/net/pinaz993/simpleshelves/client/ShelfModel.java
+++ src/main/java/net/pinaz993/simpleshelves/client/ShelfModel.java
@@ -7,25 +7,36 @@ import net.fabricmc.fabric.api.renderer.v1.mesh.Mesh;
 import net.fabricmc.fabric.api.renderer.v1.mesh.MeshBuilder;
 import net.fabricmc.fabric.api.renderer.v1.mesh.QuadEmitter;
 import net.fabricmc.fabric.api.renderer.v1.model.FabricBakedModel;
+import net.fabricmc.fabric.api.renderer.v1.render.RenderContext;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.render.model.*;
+import net.minecraft.client.render.model.json.JsonUnbakedModel;
 import net.minecraft.client.render.model.json.ModelOverrideList;
 import net.minecraft.client.render.model.json.ModelTransformation;
+import net.minecraft.client.render.model.json.Transformation;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockRenderView;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class ShelfModel implements UnbakedModel, BakedModel, FabricBakedModel {
     // The collection of all quads that need to be rendered.
+    private static final Identifier DEFAULT_BLOCK_MODEL = new Identifier("minecraft:block/block");
     private Mesh mesh;
+    private ModelTransformation transformation;
 
     @Override
     public Collection<Identifier> getModelDependencies() {
+        return List.of(DEFAULT_BLOCK_MODEL);
+
         // TODO: Figure out how to add the empty shelf model here.
     }
 
@@ -38,6 +49,10 @@ public class ShelfModel implements UnbakedModel, BakedModel, FabricBakedModel {
     @Override
     public BakedModel bake(ModelLoader loader, Function<SpriteIdentifier, Sprite> textureGetter,
                            ModelBakeSettings rotationContainer, Identifier modelId) {
+        // I need something called a transformation here. I think it has to do with the angle the item displays as in
+        // an inventory. Split into two lines like this because there's no way to cast in-line. Java is dumb.
+        JsonUnbakedModel defaultBlockModel = (JsonUnbakedModel) loader.getOrLoadModel(DEFAULT_BLOCK_MODEL);
+        transformation = defaultBlockModel.getTransformations();
         // I don't know what any of these things are. Monkey see, Monkey do.
         Renderer renderer = RendererAccess.INSTANCE.getRenderer(); // This smells. Why is there a separate access class?
         MeshBuilder builder = renderer.meshBuilder();
@@ -47,7 +62,7 @@ public class ShelfModel implements UnbakedModel, BakedModel, FabricBakedModel {
     }
 
     //<editor-fold desc="Puzzling Boilerplate">
-    // TODO: Figure out if I need to implement all three classes.
+    // TODO: Figure out if I need to implement all these classes.
     @Override
     public List<BakedQuad> getQuads(BlockState state, Direction face, Random random) {
         return null; // "Don't need because we use FabricBakedModel instead" (Press X to doubt).
@@ -64,23 +79,40 @@ public class ShelfModel implements UnbakedModel, BakedModel, FabricBakedModel {
     }
 
     @Override
-    public boolean hasDepth() { // "                 "
-        return false;
-    }
-
-    @Override
-    public boolean isSideLit() { // "                 "
-        return false;
-    }
-
-    @Override
-    public ModelTransformation getTransformation() { // Yet again, I need more information.
+    public Sprite getSprite() {
         return null;
     }
+
+    // I have no clue what this is for.
+    @Override
+    public boolean hasDepth() {return false;}
+
+    // Shelves are blocks, so render them as such in the inventory.
+    @Override
+    public boolean isSideLit() {return true;}
+
+    // I think this tells MC to render this the same as any block when in item mode.
+    @Override
+    public ModelTransformation getTransformation() {return transformation;}
 
     @Override
     public ModelOverrideList getOverrides() { //       "                                   "
         return null;
+    }
+
+    @Override
+    public boolean isVanillaAdapter() {
+        return false;
+    }
+
+    @Override
+    public void emitBlockQuads(BlockRenderView blockView, BlockState state, BlockPos pos, Supplier<Random> randomSupplier, RenderContext context) {
+
+    }
+
+    @Override
+    public void emitItemQuads(ItemStack stack, Supplier<Random> randomSupplier, RenderContext context) {
+
     }
     //</editor-fold>
 }
