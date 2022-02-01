2ff2cd1e4909277f18abd4a06e350502c5454cf4
Patrick Shannon <pinaz993@gmail.com>
2022-02-01 14:52:26 -0500

    Complied with deprecation. I do kind of like the hidden singleton method they used there.

--- src/main/java/net/pinaz993/simpleshelves/client/SimpleShelvesClient.java
+++ src/main/java/net/pinaz993/simpleshelves/client/SimpleShelvesClient.java
@@ -1,7 +1,7 @@
 package net.pinaz993.simpleshelves.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.pinaz993.simpleshelves.SimpleShelves;
 
@@ -9,7 +9,7 @@ import net.pinaz993.simpleshelves.SimpleShelves;
 public class SimpleShelvesClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        BlockEntityRendererRegistry.INSTANCE.register(
+        BlockEntityRendererRegistry.register(
                 SimpleShelves.SHELF_BLOCK_ENTITY,
                 (BlockEntityRendererFactory.Context ctx) -> new ShelfEntityRenderer()
         );
