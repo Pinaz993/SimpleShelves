0fcfa791b6d1a586658b40daa0ecfd45564904d1
Patrick Shannon <pinaz993@gmail.com>
2022-06-16 11:09:35 -0400

    Rearranged model IDs into individually accessible fields, instead of a single list.

--- src/main/java/net/pinaz993/simpleshelves/client/ShelfModelProvider.java
+++ src/main/java/net/pinaz993/simpleshelves/client/ShelfModelProvider.java
@@ -9,28 +9,31 @@ import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
+import java.util.List;
 
 @Environment(EnvType.CLIENT)
 public class ShelfModelProvider implements ModelResourceProvider {
     // A list of all the possible models this provider will have to handle.
     // Will need to be reimplemented for any addon mods.
+    // Could have just been the list, but I don't mind them being separately referencable like this.
+    public static final Identifier ACACIA_SHELF_MODEL_ID = new Identifier("simple_shelves:block/acacia_shelf");
+    public static final Identifier BIRCH_SHELF_MODEL_ID = new Identifier("simple_shelves:block/birch_shelf");
+    public static final Identifier CRIMSON_SHELF_MODEL_ID = new Identifier("simple_shelves:block/crimson_shelf");
+    public static final Identifier DARK_OAK_SHELF_MODEL_ID = new Identifier("simple_shelves:block/dark_oak_shelf");
+    public static final Identifier JUNGLE_SHELF_MODEL_ID = new Identifier("simple_shelves:block/jungle_shelf");
+    public static final Identifier OAK_SHELF_MODEL_ID = new Identifier("simple_shelves:block/oak_shelf");
+    public static final Identifier SPRUCE_SHELF_MODEL_ID = new Identifier("simple_shelves:block/spruce_shelf");
+    public static final Identifier WARPED_SHELF_MODEL_ID = new Identifier("simple_shelves:block/warped_shelf");
+
     public static final List<Identifier> MODEL_IDS = List.of(new Identifier[]{
-            new Identifier("simple_shelves:block/acacia_shelf"),
-            new Identifier("simple_shelves:block/birch_shelf"),
-            new Identifier("simple_shelves:block/crimson_shelf"),
-            new Identifier("simple_shelves:block/dark_oak_shelf"),
-            new Identifier("simple_shelves:block/jungle_shelf"),
-            new Identifier("simple_shelves:block/oak_shelf"),
-            new Identifier("simple_shelves:block/spruce_shelf"),
-            new Identifier("simple_shelves:block/warped_shelf")
+            ACACIA_SHELF_MODEL_ID, BIRCH_SHELF_MODEL_ID, CRIMSON_SHELF_MODEL_ID, DARK_OAK_SHELF_MODEL_ID,
+            JUNGLE_SHELF_MODEL_ID, OAK_SHELF_MODEL_ID, SPRUCE_SHELF_MODEL_ID, WARPED_SHELF_MODEL_ID
     });
-
-
 
     @Override
     public @Nullable UnbakedModel loadModelResource(Identifier resourceId, ModelProviderContext context) throws ModelProviderException {
-            if(MODEL_IDS.contains(resourceId)) return new ShelfModel(); // Is the resource ID one that this handles?
-            else return null; // Nope. Pass, thanks.
+        if(MODEL_IDS.contains(resourceId)) // Is this one of the models we handle?
+            return new UnbakedShelfModel(); // Return a new shelf model
+        else return null; // Nope. Pass.
     }
 }
