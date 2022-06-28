4a8117d82bd24c839195893bf3944726bd9bcb0a
Patrick Shannon <pinaz993@gmail.com>
2022-06-28 13:31:59 -0400

    Transcribed or chose starting UV coords for all book sprites. Implemented UV coord calculation in constructor.

--- src/main/java/net/pinaz993/simpleshelves/client/UnbakedBookModel.java
+++ src/main/java/net/pinaz993/simpleshelves/client/UnbakedBookModel.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.renderer.v1.mesh.QuadEmitter;
 import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec2f;
 import net.pinaz993.simpleshelves.BookPosition;
 import net.pinaz993.simpleshelves.ShelfQuadrant;
 
@@ -13,18 +14,30 @@ import java.util.List;
 
 @Environment(EnvType.CLIENT)
 public enum UnbakedBookModel {
-    ALPHA_1(BookPosition.ALPHA_1, 6.74f, 6.8f, 0b000_000_000_001),
-    ALPHA_2(BookPosition.ALPHA_2, 6.52f, 7.0f, 0b000_000_000_010),
-    ALPHA_3(BookPosition.ALPHA_3, 7.13f, 6.6f, 0b000_000_000_100),
-    BETA_1 (BookPosition.BETA_1,  6.07f, 6.5f, 0b000_000_001_000),
-    BETA_2 (BookPosition.BETA_2,  5.97f, 6.4f, 0b000_000_010_000),
-    BETA_3 (BookPosition.BETA_3,  6.25f, 7.4f, 0b000_000_100_000),
-    GAMMA_1(BookPosition.GAMMA_1, 6.99f, 5.7f, 0b000_001_000_000),
-    GAMMA_2(BookPosition.GAMMA_2, 6.62f, 6.2f, 0b000_010_000_000),
-    GAMMA_3(BookPosition.GAMMA_3, 6.79f, 6.6f, 0b000_100_000_000),
-    DELTA_1(BookPosition.DELTA_1, 5.75f, 6.0f, 0b001_000_000_000),
-    DELTA_2(BookPosition.DELTA_2, 6.76f, 7.2f, 0b010_000_000_000),
-    DELTA_3(BookPosition.DELTA_3, 6.65f, 6.2f, 0b100_000_000_000);
+    ALPHA_1(BookPosition.ALPHA_1, 6.74f, 6.8f, 0b000_000_000_001,
+            new Vec2f(0, 0), new Vec2f(6/16f, 1/16f)),
+    ALPHA_2(BookPosition.ALPHA_2, 6.52f, 7.0f, 0b000_000_000_010,
+            new Vec2f(17/16f, 0), new Vec2f(6/16f, 1/16f)),
+    ALPHA_3(BookPosition.ALPHA_3, 7.13f, 6.6f, 0b000_000_000_100,
+            new Vec2f(32/16f, 0), new Vec2f(11/16f, 1/16f)),
+    BETA_1 (BookPosition.BETA_1,  6.07f, 6.5f, 0b000_000_001_000,
+            new Vec2f(0, 7/16f), new Vec2f(20/16f, 1/16f)),
+    BETA_2 (BookPosition.BETA_2,  5.97f, 6.4f, 0b000_000_010_000,
+            new Vec2f(16/16f, 7/16f), new Vec2f(2/16f, 1/16f)),
+    BETA_3 (BookPosition.BETA_3,  6.25f, 7.4f, 0b000_000_100_000,
+            new Vec2f(33/16f, 7/16f), new Vec2f(19/16f, 1/16f)),
+    GAMMA_1(BookPosition.GAMMA_1, 6.99f, 5.7f, 0b000_001_000_000,
+            new Vec2f(0, 14/16f), new Vec2f(3/16f, 1/16f)),
+    GAMMA_2(BookPosition.GAMMA_2, 6.62f, 6.2f, 0b000_010_000_000,
+            new Vec2f(16/16f, 14/16f), new Vec2f(5/16f, 1/16f)),
+    GAMMA_3(BookPosition.GAMMA_3, 6.79f, 6.6f, 0b000_100_000_000,
+            new Vec2f(34/16f, 14/16f), new Vec2f(13/16f, 1/16f)),
+    DELTA_1(BookPosition.DELTA_1, 5.75f, 6.0f, 0b001_000_000_000,
+            new Vec2f(0, 21/16f), new Vec2f(3/16f, 1/16f)),
+    DELTA_2(BookPosition.DELTA_2, 6.76f, 7.2f, 0b010_000_000_000,
+            new Vec2f(17/17f, 21/16f), new Vec2f(10/16f, 1/16f)),
+    DELTA_3(BookPosition.DELTA_3, 6.65f, 6.2f, 0b100_000_000_000,
+            new Vec2f(33/16f, 21/16f), new Vec2f(12/16f, 1/16f));
 
     // The Z coordinate of the front surface of the rear plate of the shelf. This is as low in Z as a vertex can be.
     private static final float Z_BACK_STOP = 1 / 16f;
@@ -34,17 +47,67 @@ public enum UnbakedBookModel {
             new Identifier("simple_shelves:block/shelf_books")
     );
 
-    UnbakedBookModel(BookPosition bookPosition, float height, float depth, int bitFlag){
-        this.POSITION = bookPosition;
-        this.HEIGHT = height/16f;
-        this.DEPTH = depth/16f;
-        this.BIT_FLAG = bitFlag;
-    }
-
     public final  BookPosition POSITION;
     public final float HEIGHT; // Length in meters from top to bottom
     public final float DEPTH; // Length in meters from spine to rear shelf
     public final int BIT_FLAG; // A bit mask for use in quickly determining which books to render client-side
+    // This group of vectors represent the UV coordinates for every vertex of every quad in this model. They only need
+    // to be calculated once, so I made them final.
+    public final Vec2f HEAD_UV_V0, HEAD_UV_V1, HEAD_UV_V2, HEAD_UV_V3,
+                       REAR_UV_V0, REAR_UV_V1, REAR_UV_V2, REAR_UV_V3,
+                       SPINE_UV_V0, SPINE_UV_V1, SPINE_UV_V2, SPINE_UV_V3,
+                       FRONT_UV_V0, FRONT_UV_V1, FRONT_UV_V2, FRONT_UV_V3;
+
+    UnbakedBookModel(BookPosition bookPosition, float height, float depth, int bitFlag,
+                     Vec2f coverUpperLeftCorner, Vec2f headUpperLeftCorner){
+        this.POSITION = bookPosition;
+        this.HEIGHT = height/16f;
+        this.DEPTH = depth/16f;
+        this.BIT_FLAG = bitFlag;
+        // Calculate the UV coordinates for the sprite once per book model
+        // TODO: Separate 'shelf books' mini atlas into separate texture files for each book, ala the baking branch.
+        // Offsets:
+        final float DEPTH_TEXTURE_OFFSET = 7/16f; // The covers of all books are 7 pixels wide.
+        final float HEIGHT_TEXTURE_OFFSET = DEPTH_TEXTURE_OFFSET; // All books are as tall as their covers are wide.
+        final float FRONT_OFFSET = DEPTH_TEXTURE_OFFSET + POSITION.WIDTH; // The width of the rear cover plus the width of the spine.
+        final float RIGHT_EDGE_OFFSET = FRONT_OFFSET + DEPTH_TEXTURE_OFFSET; // The width of both covers plus the width of the spine.
+        // Defining all of those constants may seem excessive, but I am not about to allow magic numbers to creep into my code.
+
+        // Head UVs
+        this.HEAD_UV_V0 = headUpperLeftCorner; // Upper Left
+        this.HEAD_UV_V1 = new Vec2f( headUpperLeftCorner.x,
+                headUpperLeftCorner.y + HEIGHT_TEXTURE_OFFSET); // Lower Left
+        this.HEAD_UV_V2 = new Vec2f(headUpperLeftCorner.x + POSITION.WIDTH,
+                headUpperLeftCorner.y + HEIGHT_TEXTURE_OFFSET); // Lower Right
+        this.HEAD_UV_V3 = new Vec2f(headUpperLeftCorner.x + POSITION.WIDTH,
+                headUpperLeftCorner.y); // Upper Right
+        // Rear Cover UVs
+        this.REAR_UV_V0 = coverUpperLeftCorner; // Upper Left
+        this.REAR_UV_V1 = new Vec2f(coverUpperLeftCorner.x,
+                coverUpperLeftCorner.y + HEIGHT_TEXTURE_OFFSET); // Lower Left
+        this.REAR_UV_V2 = new Vec2f(coverUpperLeftCorner.x + DEPTH_TEXTURE_OFFSET,
+                coverUpperLeftCorner.y + HEIGHT_TEXTURE_OFFSET); // Lower Right
+        this.REAR_UV_V3 = new Vec2f(coverUpperLeftCorner.x + DEPTH_TEXTURE_OFFSET,
+                coverUpperLeftCorner.y); // Upper Right
+        // Spine UVs
+        this.SPINE_UV_V0 = new Vec2f(coverUpperLeftCorner.x + DEPTH_TEXTURE_OFFSET,
+                coverUpperLeftCorner.y); // Upper Left
+        this.SPINE_UV_V1 = new Vec2f(coverUpperLeftCorner.x + DEPTH_TEXTURE_OFFSET,
+                coverUpperLeftCorner.y + HEIGHT_TEXTURE_OFFSET); // Lower Left
+        this.SPINE_UV_V2 = new Vec2f(coverUpperLeftCorner.x+ FRONT_OFFSET,
+                coverUpperLeftCorner.y + HEIGHT_TEXTURE_OFFSET); // Lower Right
+        this.SPINE_UV_V3 = new Vec2f(coverUpperLeftCorner.x + FRONT_OFFSET,
+                coverUpperLeftCorner.y); // Upper Right
+
+        this.FRONT_UV_V0 = new Vec2f(coverUpperLeftCorner.x + FRONT_OFFSET,
+                coverUpperLeftCorner.y);// Upper Left
+        this.FRONT_UV_V1 = new Vec2f(coverUpperLeftCorner.x + FRONT_OFFSET,
+                coverUpperLeftCorner.y + HEIGHT_TEXTURE_OFFSET); // Lower Left
+        this.FRONT_UV_V2 = new Vec2f(coverUpperLeftCorner.x + RIGHT_EDGE_OFFSET,
+                coverUpperLeftCorner.y + HEIGHT_TEXTURE_OFFSET); // Lower Right
+        this.FRONT_UV_V3 = new Vec2f(coverUpperLeftCorner.x + RIGHT_EDGE_OFFSET,
+                coverUpperLeftCorner.y); // Upper Right
+    }
 
 
     public void emitBookQuads(QuadEmitter e) {
@@ -62,7 +125,13 @@ public enum UnbakedBookModel {
         e.pos(2, right, HEIGHT, DEPTH);
         e.pos(3, right, HEIGHT, Z_BACK_STOP);
         // Sprite work:
-        e.spriteBake()
+        // Set the UV coords
+
+
+        // Enable texture usage
+        e.spriteColor(0, -1, -1, -1, -1);
+        // Add the quad to the mesh
+        e.emit();
 
         // Next, the rear cover.
         e.pos(0, left, HEIGHT, Z_BACK_STOP);
@@ -71,6 +140,11 @@ public enum UnbakedBookModel {
         e.pos(3, left, HEIGHT, DEPTH);
         // Sprite work:
 
+        // Enable texture usage
+        e.spriteColor(0, -1, -1, -1, -1);
+        // Add the quad to the mesh
+        e.emit();
+
         // Now, the spine.
         e.pos(0, left, HEIGHT, DEPTH);
         e.pos(1, left, shelf, DEPTH);
@@ -78,11 +152,21 @@ public enum UnbakedBookModel {
         e.pos(3, right, HEIGHT, DEPTH);
         // Sprite work:
 
+        // Enable texture usage
+        e.spriteColor(0, -1, -1, -1, -1);
+        // Add the quad to the mesh
+        e.emit();
+
         // Finally, the front cover.
         e.pos(0, right, HEIGHT, DEPTH);
         e.pos(1, right, shelf, DEPTH);
         e.pos(2, right, shelf, Z_BACK_STOP);
         e.pos(3, right, HEIGHT, Z_BACK_STOP);
         // Sprite work
+
+        // Enable texture usage
+        e.spriteColor(0, -1, -1, -1, -1);
+        // Add the quad to the mesh
+        e.emit();
     }
 }
