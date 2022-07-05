ad503a3a2d309ec8262041ede7460a7bdd5d0953
Patrick Shannon <pinaz993@gmail.com>
2022-07-05 11:55:05 -0400

    Refactored psf model identifiers to ModelIdentifier class. Changed loadModelResource() to fail fast behavior and include blockstate variant identifiers.

--- src/main/java/net/pinaz993/simpleshelves/client/ShelfModelProvider.java
+++ src/main/java/net/pinaz993/simpleshelves/client/ShelfModelProvider.java
@@ -6,7 +6,9 @@ import net.fabricmc.fabric.api.client.model.ModelProviderContext;
 import net.fabricmc.fabric.api.client.model.ModelProviderException;
 import net.fabricmc.fabric.api.client.model.ModelResourceProvider;
 import net.minecraft.client.render.model.UnbakedModel;
+import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.util.Identifier;
+import net.pinaz993.simpleshelves.SimpleShelves;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -16,24 +18,30 @@ public class ShelfModelProvider implements ModelResourceProvider {
     // A list of all the possible models this provider will have to handle.
     // Will need to be reimplemented for any addon mods.
     // Could have just been the list, but I don't mind them being separately referencable like this.
-    public static final Identifier ACACIA_SHELF_MODEL_ID = new Identifier("simple_shelves:block/acacia_shelf");
-    public static final Identifier BIRCH_SHELF_MODEL_ID = new Identifier("simple_shelves:block/birch_shelf");
-    public static final Identifier CRIMSON_SHELF_MODEL_ID = new Identifier("simple_shelves:block/crimson_shelf");
-    public static final Identifier DARK_OAK_SHELF_MODEL_ID = new Identifier("simple_shelves:block/dark_oak_shelf");
-    public static final Identifier JUNGLE_SHELF_MODEL_ID = new Identifier("simple_shelves:block/jungle_shelf");
-    public static final Identifier OAK_SHELF_MODEL_ID = new Identifier("simple_shelves:block/oak_shelf");
-    public static final Identifier SPRUCE_SHELF_MODEL_ID = new Identifier("simple_shelves:block/spruce_shelf");
-    public static final Identifier WARPED_SHELF_MODEL_ID = new Identifier("simple_shelves:block/warped_shelf");
+    public static final ModelIdentifier ACACIA_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/acacia_shelf");
+    public static final ModelIdentifier BIRCH_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/birch_shelf");
+    public static final ModelIdentifier CRIMSON_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/crimson_shelf");
+    public static final ModelIdentifier DARK_OAK_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/dark_oak_shelf");
+    public static final ModelIdentifier JUNGLE_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/jungle_shelf");
+    public static final ModelIdentifier OAK_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/oak_shelf");
+    public static final ModelIdentifier SPRUCE_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/spruce_shelf");
+    public static final ModelIdentifier WARPED_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/warped_shelf");
+    // An identifier for the texture I use for books.
+    public static final Identifier SHELF_BOOK_TEXTURE_ID = new Identifier("simple_shelves:block/shelf_books");
 
-    public static final List<Identifier> MODEL_IDS = List.of(new Identifier[]{
+    public static final List<ModelIdentifier> MODEL_IDS = List.of(new ModelIdentifier[]{
             ACACIA_SHELF_MODEL_ID, BIRCH_SHELF_MODEL_ID, CRIMSON_SHELF_MODEL_ID, DARK_OAK_SHELF_MODEL_ID,
             JUNGLE_SHELF_MODEL_ID, OAK_SHELF_MODEL_ID, SPRUCE_SHELF_MODEL_ID, WARPED_SHELF_MODEL_ID
     });
 
     @Override
     public @Nullable UnbakedModel loadModelResource(Identifier resourceId, ModelProviderContext context) throws ModelProviderException {
-        if(MODEL_IDS.contains(resourceId)) // Is this one of the models we handle?
-            return new UnbakedShelfModel(); // Return a new shelf model
-        else return null; // Nope. Pass.
+        if (resourceId.getNamespace().equals(SimpleShelves.NAMESPACE)) { // Is the ID from this mod?
+            for(ModelIdentifier mi: MODEL_IDS) {// Iterate through all the model IDs
+                if (resourceId.getPath().equals(mi.getPath())) // Does the ID in question have the same path as the current model ID?
+                    return new UnbakedShelfModel(); // Yes. Return a new shelf model.
+            }
+        }
+        return null; // Nope. Pass.
     }
 }
