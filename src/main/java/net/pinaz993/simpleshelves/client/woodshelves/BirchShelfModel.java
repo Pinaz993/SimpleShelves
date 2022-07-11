26651715cdc2e6a2f0bbf6801546c5a57cf6d79e
Patrick Shannon <pinaz993@gmail.com>
2022-07-11 10:19:29 -0400

    Created implementations for UnbakedShelfModel that provide a texture to use for the shelf itself.

---
+++ src/main/java/net/pinaz993/simpleshelves/client/woodshelves/BirchShelfModel.java
@@ -0,0 +1,16 @@
+package net.pinaz993.simpleshelves.client.woodshelves;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.screen.PlayerScreenHandler;
+import net.minecraft.util.Identifier;
+import net.pinaz993.simpleshelves.client.UnbakedShelfModel;
+
+@Environment(EnvType.CLIENT)
+public class BirchShelfModel extends UnbakedShelfModel {
+    @Override
+    public SpriteIdentifier getShelfSpriteID() {
+        return new SpriteIdentifier(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE, new Identifier("minecraft:block/birch_planks"));
+    }
+}
