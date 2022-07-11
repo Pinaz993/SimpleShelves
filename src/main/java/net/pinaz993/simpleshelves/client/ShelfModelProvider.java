b7f44240cbbe1726170efb0daa693715469ec260
Patrick Shannon <pinaz993@gmail.com>
2022-07-11 11:41:22 -0400

    Cleaned up block identification, and implemented block model resource detection.

--- src/main/java/net/pinaz993/simpleshelves/client/ShelfModelProvider.java
+++ src/main/java/net/pinaz993/simpleshelves/client/ShelfModelProvider.java
@@ -9,40 +9,31 @@ import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.util.Identifier;
 import net.pinaz993.simpleshelves.SimpleShelves;
+import net.pinaz993.simpleshelves.client.woodshelves.*;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 @Environment(EnvType.CLIENT)
 public class ShelfModelProvider implements ModelResourceProvider {
     // A list of all the possible models this provider will have to handle.
     // Will need to be reimplemented for any addon mods.
     // Could have just been the list, but I don't mind them being separately referencable like this.
-    public static final ModelIdentifier ACACIA_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/acacia_shelf");
-    public static final ModelIdentifier BIRCH_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/birch_shelf");
-    public static final ModelIdentifier CRIMSON_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/crimson_shelf");
-    public static final ModelIdentifier DARK_OAK_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/dark_oak_shelf");
-    public static final ModelIdentifier JUNGLE_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/jungle_shelf");
-    public static final ModelIdentifier OAK_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/oak_shelf");
-    public static final ModelIdentifier SPRUCE_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/spruce_shelf");
-    public static final ModelIdentifier WARPED_SHELF_MODEL_ID = new ModelIdentifier("simple_shelves:block/warped_shelf");
     // An identifier for the texture I use for books.
     public static final Identifier SHELF_BOOK_TEXTURE_ID = new Identifier("simple_shelves:block/shelf_books");
-
-    public static final List<ModelIdentifier> MODEL_IDS = List.of(new ModelIdentifier[]{
-            ACACIA_SHELF_MODEL_ID, BIRCH_SHELF_MODEL_ID, CRIMSON_SHELF_MODEL_ID, DARK_OAK_SHELF_MODEL_ID,
-            JUNGLE_SHELF_MODEL_ID, OAK_SHELF_MODEL_ID, SPRUCE_SHELF_MODEL_ID, WARPED_SHELF_MODEL_ID
-    });
 
     @Override
     public @Nullable UnbakedModel loadModelResource(Identifier resourceId, ModelProviderContext context) throws ModelProviderException {
         if (resourceId.getNamespace().equals(SimpleShelves.NAMESPACE)) { // Is the ID from this mod?
-            for(ModelIdentifier mi: MODEL_IDS) {// Iterate through all the model IDs
-                if (resourceId.getPath().equals(mi.getPath())) // Does the ID in question have the same path as the current model ID?
-                    return new UnbakedShelfModel(); // Yes. Return a new shelf model.
-                else if (resourceId.getPath().contains("item")) // Does the path indicate that this is an item model?
-                    return new UnbakedShelfModel(); // Yes. Return a new shelf model.
-            }
+            return switch (resourceId.getPath().split("/")[1]) {       // Here, we're grabbing the String 'path'
+                case "acacia_shelf" -> new AcaciaShelfModel();         // ("block/oak_shelf") from the resource ID,
+                case "birch_shelf" -> new BirchShelfModel();           // which we've already ensured comes from this
+                case "crimson_shelf" -> new CrimsonShelfModel();       // mod. Then, we're using regex to split that
+                case "dark_oak_shelf" -> new DarkOakShelfModel();      // String into an array of Strings
+                case "jungle_shelf"-> new JungleShelfModel();          // {"block", "oak_shelf"}, which we then take the
+                case "oak_shelf" -> new OakShelfModel();               // second entry in ("oak_shelf") so that we can
+                case "spruce_shelf" -> new SpruceShelfModel();         // see which block/item we need to make a model
+                case "warped_shelf" -> new WarpedShelfModel();         // for. If it doesn't fall into one of these
+                default -> null;                                       // entries, we return null to let the game know
+            };                                                         // to fall back to the vanilla system.
         }
         return null; // Nope. Pass.
     }
