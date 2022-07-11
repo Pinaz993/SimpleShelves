03bd032cd691b668a64e809db0a97212c32d090f
Patrick Shannon <pinaz993@gmail.com>
2022-07-11 11:32:21 -0400

    Fixed improper offsets causing inside-out rendering, but only for book Alpha 1. More work needed here.

--- src/main/java/net/pinaz993/simpleshelves/client/BakedShelfModel.java
+++ src/main/java/net/pinaz993/simpleshelves/client/BakedShelfModel.java
@@ -2,6 +2,7 @@ package net.pinaz993.simpleshelves.client;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.renderer.v1.mesh.Mesh;
 import net.fabricmc.fabric.api.renderer.v1.model.FabricBakedModel;
 import net.fabricmc.fabric.api.renderer.v1.render.RenderContext;
 import net.fabricmc.fabric.api.rendering.data.v1.RenderAttachedBlockView;
@@ -10,10 +11,13 @@ import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.BakedQuad;
 import net.minecraft.client.render.model.json.ModelOverrideList;
 import net.minecraft.client.render.model.json.ModelTransformation;
+import net.minecraft.client.render.model.json.Transformation;
+import net.minecraft.client.texture.MissingSprite;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3f;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.BlockRenderView;
 import net.pinaz993.simpleshelves.BookPosition;
@@ -26,6 +30,66 @@ import java.util.function.Supplier;
 
 @Environment(EnvType.CLIENT)
 public class BakedShelfModel implements FabricBakedModel, BakedModel {
+    // Controls how the shelves are rendered in different non-block contexts. Usually defined in JSON.
+    //FIXME: Item forms are WAY too offset.
+    private static final ModelTransformation TRANSFORMATION = new ModelTransformation(
+            new Transformation( // 3PP Left Hand
+                    new Vec3f(75f, -45f, 0f),
+                    new Vec3f(0f, 2.5f, 0),
+                    new Vec3f(.375f, .375f, .375f)
+            ),
+            new Transformation( // 3PP Right Hand
+                    new Vec3f(75f, -45f, 0f),
+                    new Vec3f(0f, 2.5f, 0),
+                    new Vec3f(.375f, .375f, .375f)
+            ),
+            new Transformation( // 1PP Left Hand
+                    new Vec3f(0f, 45f, 0f),
+                    new Vec3f(0f, 2.5f, 0),
+                    new Vec3f(.4f, .4f, .4f)
+            ),
+            new Transformation( // 1PP Left Hand
+                    new Vec3f(0f, 45f, 0f),
+                    new Vec3f(0f, 2.5f, 0),
+                    new Vec3f(.4f, .4f, .4f)
+            ),
+            new Transformation( // Head
+                    new Vec3f(0f, 0f, 0f),
+                    new Vec3f(0f, 0f, .5f),
+                    new Vec3f(1f, 1f, 1f)
+            ),
+            new Transformation( // GUI
+                    new Vec3f(30f, 135f, 0f),
+                    new Vec3f(-1.5f, -1f, 0f),
+                    new Vec3f(.625f, .625f, .625f)
+            ),
+            new Transformation( // Ground
+                    new Vec3f(0f, 0f, 0f),
+                    new Vec3f(0f, 3f, 0f),
+                    new Vec3f(.25f, .25f, .25f)
+            ),
+            new Transformation( // Fixed, or Item Frame
+                    new Vec3f(0f, 0f, 0f),
+                    new Vec3f(0f, 0f, -4f),
+                    new Vec3f(.5f, .5f, .5f)
+            )
+    );
+
+    private final Mesh TEMP_BOOK_MESH;
+    private final Sprite SHELF_SPRITE;
+
+    //private final Mesh SHELF_MESH;
+    //TODO: Implement members for all 12 book meshes.
+
+    public BakedShelfModel(Mesh tbm, Sprite shelfSprite){
+        this.SHELF_SPRITE = shelfSprite;
+        this.TEMP_BOOK_MESH = tbm;
+
+        //this.SHELF_MESH = sm;
+
+
+    }
+
     @Override
     public boolean isVanillaAdapter() {
         return false;
@@ -33,23 +97,19 @@ public class BakedShelfModel implements FabricBakedModel, BakedModel {
 
     @Override
     public void emitBlockQuads(BlockRenderView blockView, BlockState state, BlockPos pos, Supplier<Random> randomSupplier, RenderContext context) {
-        int renderData = (int) ((RenderAttachedBlockView) blockView).getBlockEntityRenderAttachment(pos);
+        context.meshConsumer().accept(TEMP_BOOK_MESH);
+
+        //FIXME: For now, we're just going to accept all the book meshes, so I can get them all squared away and rendering correctly.
+
+
+        // int renderData = (int) ((RenderAttachedBlockView) blockView).getBlockEntityRenderAttachment(pos);
+        // I'll need that later.
         //TODO: Implement bit flag logic, referencing BookPosition bit flags.
-
-        for(BookPosition bp: BookPosition.class.getEnumConstants()){
-            if(true) { //TODO: Fill in logic for determining if a book needs to be modeled.
-
-            }
-        }
-
-        // It looks like actually rendering the books will be as simple as context.meshConsumer().accept(mesh).
-        // Same for the shelf. It doesn't look like I'll be able to modify any of the meshes here. That will have to be
-        // done in UnbakedShelfModel.
     }
 
     @Override
     public void emitItemQuads(ItemStack stack, Supplier<Random> randomSupplier, RenderContext context) {
-
+        context.meshConsumer().accept(TEMP_BOOK_MESH);
     }
 
     @Override
@@ -79,16 +139,16 @@ public class BakedShelfModel implements FabricBakedModel, BakedModel {
 
     @Override
     public Sprite getParticleSprite() {
-        return null;
+        return SHELF_SPRITE;
     }
 
     @Override
     public ModelTransformation getTransformation() {
-        return null;
+        return TRANSFORMATION;
     }
 
     @Override
     public ModelOverrideList getOverrides() {
-        return null;
+        return ModelOverrideList.EMPTY;
     }
 }
