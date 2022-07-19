12fbc8e9653dd2dc8dddda9709408a6f756d831e
Patrick Shannon <pinaz993@gmail.com>
2022-07-19 11:55:50 -0400

    Forgot the client-side only annotation.

--- src/main/java/net/pinaz993/simpleshelves/client/woodshelves/MangroveShelfModel.java
+++ src/main/java/net/pinaz993/simpleshelves/client/woodshelves/MangroveShelfModel.java
@@ -1,11 +1,14 @@
 package net.pinaz993.simpleshelves.client.woodshelves;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
 import net.pinaz993.simpleshelves.SimpleShelves;
 import net.pinaz993.simpleshelves.client.UnbakedShelfModel;
 
+@Environment(EnvType.CLIENT)
 public class MangroveShelfModel extends UnbakedShelfModel {
     @Override
     public SpriteIdentifier getShelfSpriteId() {
